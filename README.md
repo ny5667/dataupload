e6462862292e57fe9663a6e41178614cd8be8e81
NY5667 <82657677+NY5667@users.noreply.github.com>
2023-09-11 15:16:54 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dataupload
 长治园区环保数据实时上报
+
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=ny5667_dataupload&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=ny5667_dataupload)
--- src/main/java/com/supcon/ses/dataupload/utils/FileHelper.java
+++ src/main/java/com/supcon/ses/dataupload/utils/FileHelper.java
@@ -1,20 +1,14 @@
 package com.supcon.ses.dataupload.utils;
 
 import com.supcon.ses.dataupload.exceptions.FileReadException;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileHelper {
 
-    private FileHelper(){
+    private FileHelper() {
 
     }
 
@@ -24,7 +18,7 @@ public class FileHelper {
             return new String(bytes);
         } catch (IOException e) {
             // 处理读取文件异常
-            throw new FileReadException("文件读取报错",e);
+            throw new FileReadException("文件读取报错", e);
         }
     }
 
